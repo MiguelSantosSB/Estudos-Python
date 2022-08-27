69b5cc7be66c060c25c740427bb71122fff972ef
iDtc Solution!!! <miguel.santoss085@gmail.com>
2022-08-27 00:20:55 -0300

    commit

--- Seção_2/aula23.py
+++ Seção_2/aula23.py
@@ -1,3 +1,13 @@
-nome = input('Qual seu nome ?\n')
+"""
+nome = input('Qual seu nome ? \n')
+idade = input('Qual sua idade ? \n')
+# o input retorna sempre uma string então e preciso tranformar em int
+ano = 2022 - int(idade)
 
-print(nome)
+print(f'Seu nome e: {nome} 'f'{type(nome)}')
+print(f'seu ano de nascimento e : {ano} 'f'{type(ano)}')
+"""
+num1 = int(input('Digite um número: '))
+num2 = int(input('Digite outro número: '))
+
+print(num1 + num2)
