90b0f743c5569d84b33cb513336e17eb63e8c766
Miguel Santos <111910150+MiguelSantosSB@users.noreply.github.com>
2022-08-29 11:14:18 -0500

    Create aula30_2.py

---
+++ Seção_2/aula30_2.py
@@ -0,0 +1,63 @@
+"""
+# 1o
+# Faça um programa que peça ao usuário para digitar um número inteiro,
+# informe se este número é par ou ímpar. Caso o usuário não digite um número
+# inteiro, informe que não é um número inteiro.
+
+num = input('Digite um número inteiro: ')
+
+
+if num.isnumeric():
+        #a = isinstance(num, int)
+        #if a;
+    num = int(num)
+    if num % 2 == 0:
+        print('Seu número e Par!')
+    elif num % 1 == 0:
+            print('Seu número e Ímpar!')
+else:
+    print('Seu número não e inteiro!')
+# perguntar depois a questão dos números negativos
+"""
+
+# ------------------------------------------------------------------------------
+
+# 2o
+# Faça um programa que pergunte a hora ao usuário e,baseando-se no horário
+# descrito, exiba a saudação apropriada.
+
+"""
+hora = input('Digite a atual hora: ')
+existe = hora.isnumeric()
+try:
+    hora = int(hora)
+    if existe == True:
+        if hora >= 0 and 11 >= hora:
+            print('Bom dia!')
+        elif hora >= 12 and 17 >= hora:
+            print('Boa tarde!')
+        elif hora >= 18 and 23 >= hora:
+            print('Boa noite!')
+        else:
+            print('Valor digitado não existe uma hora correspondente ')
+except:
+    print('O valor digitado não é um número natural')
+"""
+
+# ------------------------------------------------------------------------------
+
+"""
+# 3o
+# Faça um programa que peça o primeiro nome do usuário. Se o nome tiver 4 letras ou
+# menos escreva "seu nome é curto"; se tiver entre 5 e 6 letra, escreva 
+# "seu nome é normal";maior que 6 escreva "seu nome é muito grande"
+
+nome = input('Digite seu primeiro nome: ')
+letras = len(nome)
+if letras <= 4:
+    print('Seu nome é muito curto')
+elif letras >= 5 and 6 >= letras:
+    print('Seu nome é normal')
+elif letras > 6:
+    print('Seu nome é muito grande')
+"""
