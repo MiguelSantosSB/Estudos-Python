01c88eb40ddab97a08f9250c19cbc672b48cc69f
Miguel Santos <111910150+MiguelSantosSB@users.noreply.github.com>
2022-09-02 15:46:50 -0300

    Create 46.py

---
+++ Seção_2/46.py
@@ -0,0 +1,16 @@
+"""
+Expressão condicional com operador OR
+"""
+
+nome = input('Qual seu nome ?')
+
+"""
+if nome:
+    print(nome)
+else:
+    print('Você não digitou nada.')
+"""
+
+# A mesma coisa:
+
+print(nome or 'Você não digitou nada')
