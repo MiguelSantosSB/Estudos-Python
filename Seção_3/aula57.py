eba92c66c64b9cd253d1c39b941cc6120405bc29
Miguel Santos <111910150+MiguelSantosSB@users.noreply.github.com>
2022-09-08 15:36:55 -0300

    Create aula57.py

---
+++ Seção_3/aula57.py
@@ -0,0 +1,4 @@
+# 1- Crie um função1 que recebe uma função2 como parâmetro e retorne o valor da função2 executada.
+
+# 2- Crie uma função1 que receba uma função2 como parâmetro e retorne o valor da função2 executada.
+#    Faça a função1 executar duas funções que recebam um número diferente de argumentos.
