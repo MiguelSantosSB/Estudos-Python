7669a16394c11db760203ed3f28b78b31ccf0b6a
Miguel Santos <111910150+MiguelSantosSB@users.noreply.github.com>
2022-09-08 15:18:33 -0300

    Create aula55.py

---
+++ Seção_3/aula55.py
@@ -0,0 +1,17 @@
+"""
+Funções (def) - *args **kwargs -
+"""
+
+def teste(*args, **kwargs):
+    print(args)
+
+    idade = kwargs.get('idade')
+    print(idade)
+    print(kwargs['nome'])
+
+    # print(args[0])
+    # print(*args)
+
+lista = [1,2,3,4,5]
+# teste(1,2,3,4,5)
+teste(*lista, nome = 'miguel', idade = 31)
