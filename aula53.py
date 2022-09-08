60196e8a042ef9bc165c4edd26fc272e15a6dc1f
Miguel Santos <111910150+MiguelSantosSB@users.noreply.github.com>
2022-09-08 13:38:37 -0300

    Add files via upload

---
+++ aula53.py
@@ -0,0 +1,18 @@
+"""def soma(x, y):
+    soma = x + y
+    return soma 
+
+
+a = 10
+b = 34
+
+result = soma(a, b)
+print(result)"""
+
+
+def dumb():
+    return ('Miguel', 'Santos')
+
+teste = dumb()
+print(teste[0],type(teste))
+print(teste[1],type(teste))
---
+++ aula54.py
@@ -0,0 +1,73 @@
+# 1- Crie uma função que exibe uma saudação com os parâmetros saudação e nome.
+ 
+# def teste(saudacao, nome):
+#     print(f'{saudacao}, {nome}')
+    
+# name = input('Qual seu nome ?')    
+
+# horas = input('Que horas são ?')
+# horas = int(horas)
+# if horas > 00 and horas < 12:
+#     horas = 'Bom dia'
+# elif horas > 12 and horas < 18:
+#     horas = 'Boa tarde'
+# elif horas > 18 and horas < 24:
+#     horas = 'Boa noite'
+
+# teste(horas, name)
+
+# 2- Crie uma função que recebe 3 números com parâmetros e exiba a soma entre eles.
+
+# def teste(num0,num1,num2):
+
+#     return num0 + num1 + num2
+
+# dado = input('Digite 3 números: ')
+# dado1 = input()
+# dado2 = input()
+
+# dado = int(dado)
+# dado1 = int(dado1)
+# dado2 = int(dado2)
+
+# print(teste(dado,dado1,dado2))
+
+# 3- Crie uma função que receba 2 números. O primeiro é um valor e o segundo um percentual(ex: 10%)
+# retorne(return) o valor do primeiro número somado do aumento do percentual do mesmo.
+
+# def teste(num,porc):
+
+#     porc = porc / 100
+#     porc2 = num * porc
+#     num = num + porc2
+
+#     return num
+
+# dado = input('Digite 2 números: ')
+# dado2 = input()
+
+# dado = int(dado)
+# dado2 = int(dado2)
+
+# print(teste(dado, dado2))
+
+# 4- Fizz Buzz - Se o parâmetro da função for divisível por 3, retorne fizz, se o parâmetro da 
+# função for divisível por 5, retorne buzz. Se o parâmetro da função for divisível por 5 e por 3, 
+# retorne FizzBuzz, caso contrário, retorne o número enviado.
+
+# def teste(num):
+#     if num % 3 == 0 and num % 5 == 0:
+#         return 'FizzBuzz'
+
+#     if num % 5 == 0:
+#         return 'Buzz'
+
+#     if num % 3 == 0:
+#         return 'Fizz'
+
+#     return num
+
+# dado = input('Digite um número: ')
+# dado = int(dado)
+
+# print(teste(dado))
