59fc5ea427bb6f0926b32989fd30d9145423af12
Miguel Santos <111910150+MiguelSantosSB@users.noreply.github.com>
2022-09-08 14:13:40 -0300

    Create aula52.py

---
+++ Seção_3/aula52.py
@@ -0,0 +1,21 @@
+"""
+função (def) - return
+"""
+
+"""
+def funcao(name):
+    return name
+variavel = funcao('valor que eu quero')
+if variavel:
+    print(variavel)
+else:
+    print('Nenhum valor encontrado')
+"""
+
+def divisao(n1, n2):
+    if n2 > 0:
+
+        return int(n1 / n2)
+
+divide = divisao(8,2)
+print(divide)
