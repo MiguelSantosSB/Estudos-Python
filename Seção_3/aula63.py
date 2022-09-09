ec74d372121a34aabd04d45997a7b20467a978e8
Miguel Santos <111910150+MiguelSantosSB@users.noreply.github.com>
2022-09-09 15:46:03 -0300

    Create aula63.py

---
+++ Seção_3/aula63.py
@@ -0,0 +1,23 @@
+"""
+add(adicioan), update(atualiza), clear, discard
+union | (une)
+intersection & (todos os elementos presentes nos dois sets)
+difference - (elementos apeas no set da esquerda)
+symmetric_difference ^ (elementos que estão nos dois sets, mas não em ambos)
+"""
+
+# diferença entre os sets e as listas:
+# eles(sets) só suportam elementos unicos, só recebem valores no caso
+# eles não tem indice então não tem como acessar um valor especifico do set
+
+set1 = {1,2,3,4,5}
+
+# print(set1)
+
+l1 = [1,2,3,4,5,6,6,6,6,6,6,6,7,8,9,'luiz','luiz']
+# transforma em um set, removendo os elementos repetidos
+l1 = set(l1)
+print(l1)
+# transforma de volta para lista e sem os itens repetidos
+l1 = list(l1)
+print(l1)
