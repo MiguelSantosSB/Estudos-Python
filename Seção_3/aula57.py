aa439938d48967b52780134abb62d6a9b5eb7f1f
Miguel Santos <111910150+MiguelSantosSB@users.noreply.github.com>
2022-09-09 12:37:39 -0300

    Update aula57.py

--- Seção_3/aula57.py
+++ Seção_3/aula57.py
@@ -1,4 +1,29 @@
-# 1- Crie um função1 que recebe uma função2 como parâmetro e retorne o valor da função2 executada.
+# 1- Crie uma função1 que recebe uma função2 como parâmetro e retorne o valor da função2 executada.
+
+# def funcao1():
+#     return 'teste teste...'
+
+# def funcao2(funcao):
+#     return funcao()
+
+# exec = funcao2(funcao1)
+# print(exec)
+# CORRIGIDA
 
 # 2- Crie uma função1 que receba uma função2 como parâmetro e retorne o valor da função2 executada.
 #    Faça a função1 executar duas funções que recebam um número diferente de argumentos.
+
+# def nome(name):
+#     return f'Olá {name}'
+
+# def sad(name, hi):
+#     return f'{hi} {name}'
+
+# def juntos(funcao, *args, **kwargs):
+#     return funcao(*args, **kwargs)
+
+# teste = juntos(nome, 'Lucas')
+# teste2 = juntos(sad, 'Lucas','Bom dia')
+
+# print(teste)
+# print(teste2)
