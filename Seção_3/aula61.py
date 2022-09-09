ba70ba8014cf58489733606b1ad57fed8ca5582a
Miguel Santos <111910150+MiguelSantosSB@users.noreply.github.com>
2022-09-09 14:50:00 -0300

    Update aula61.py

--- Seção_3/aula61.py
+++ Seção_3/aula61.py
@@ -1,1 +1,43 @@
+"""
+Dicionário em Python
+"""
+
+d1 = {'chave1':'valor da chave'}
+d1['nova_chave'] = 'valor da nova chave'
+
+# print(d1['chave1'])
+
+d2 = {
+    'str' : 'valor',
+    123: 'outro valor',
+    (1,2,3,4) : 'Tupla'
+}
+
+# print(d2['str'])
+# print(d2[123])
+# print(d2[(1,2,3,4)])
+
+# adiciona algo no dicionário 
+# d2.update({'chave':'ehhhh'})
+
+# if d2.get('chave') is not None:
+#     print(d2.get('chave'))
+# print(d2['chave'])
+
+# del d2[123]
+
+# print(d2)
+
+# verifica se o que está entre aspas está presente no dicionário
+# print('str' in d2.keys())
+
+# verifica se o que está entre aspas está presente nos valores dentro do dicionário 
+# print('valor' in d2.values())
+
+# -------------------------------------------------------------------------------------------------------------------
+
+# somando um dicionario com outro
+
+d1.update(d2)
+# print(d1)
 
