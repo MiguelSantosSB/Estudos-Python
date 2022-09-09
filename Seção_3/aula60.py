92aabbe753bd6543390486cc2c9e8c49ee30c300
Miguel Santos <111910150+MiguelSantosSB@users.noreply.github.com>
2022-09-09 13:30:13 -0300

    Create aula60.py

---
+++ Seção_3/aula60.py
@@ -0,0 +1,38 @@
+"""
+Expressões Lambda(funções anônimas)
+"""
+
+# def funcao(arg, arg2):
+#     return arg * arg2
+
+# var = funcao(2,2)
+# print(var)
+
+# mesma coisa de:
+
+# a = lambda x,y: x * y
+
+# print(a(2,2))
+
+lista = [
+    ['P1', 22],
+    ['P2', 66],
+    ['P3', 4],
+    ['P4', 33],
+    ['P5', 20],
+]
+# irá receber o item e retornar na chave desejada
+def func(item):
+    return item[1]
+
+# irá ordenar de acordo com a chave desejada
+# do menor ao maior nesse caso 
+# lista.sort(key = func)
+# utilizando a lambda 
+lista.sort(key = lambda item : item[1])
+
+
+# nesse caso irá ser do meior para o menor 
+# lista.sort(key = func, reverse = True)
+
+print(lista)
