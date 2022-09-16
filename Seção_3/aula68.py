cd817e847c5ff5ff2224774652056851316620b8
MiguelSantosSB <miguel.santoss085@gmail.com>
2022-09-16 12:45:22 -0300

    exercício proposto 68

--- Seção_3/aula68.py
+++ Seção_3/aula68.py
@@ -5,15 +5,19 @@ lista = ['0123456789', '0123456789', '0123456789']
 e depois fazer isso:
 retorno = '0123456789.0123456789.0123456789.0123456789.0123456789'
 """
-
+# MINHA TENTATIVA:
 string = '012345678901234567890123456789012345678901234567890123456789'
 
-teste = string.split(string[0:10:10])
+for i in range(0,len(string), 10):
+    teste = [string[i:i+10]]
+    teste = list(teste)
 
-# del teste[0]
+nova = teste[0:10]
 
-
-print(teste)
+a = 0
+while a < 5:
+    teste += nova
+    a += 1
 
 teste = '.'.join(teste)
-# print(teste)
+print(teste)
