e7b9e9137fa0f018e9c9b4f45f9946790c58d80f
joselisboaa <miguel.santoss085@gmail.com>
2022-09-20 13:53:26 -0300

    Exercício proposto

---
+++ Seção_3/aula73.py
@@ -0,0 +1,9 @@
+carrinho = []
+
+carrinho.append(('produto 1', 30))
+carrinho.append(('produto 2', 20))
+carrinho.append(('produto 3', 50))
+
+teste = [(y) for x, y in carrinho]
+teste = sum(list(teste))
+print(teste)
