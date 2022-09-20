58be8c3c9a7f855e7fd1bdfb7fa70f351c61a536
joselisboaa <miguel.santoss085@gmail.com>
2022-09-20 15:11:33 -0300

    Zip e Zip_longest

--- Seção_3/aula75.py
+++ Seção_3/aula75.py
@@ -13,14 +13,14 @@ estados = ['SP', 'MG', 'BA']
 cidades_estados = zip(cidades, estados)
 
 print(list(cidades_estados))
-# o Zip une 2 litas.
-# se as listas não forem iguais a menor lista irá determinar o tamanho do zip
+# o Zip une 2 listas.
+# se as listas não forem iguais a menor lista irá determinar o tamanho do zip.
 
 cidades_estados = zip_longest(cidades, estados, fillvalue='estado')
 print(list(cidades_estados))
 
-# no caso do Zip_longest o limitador dele e a maior lista
-# para utilizar esse zip e preciso importar 
+# no caso do Zip_longest o limitador dele e a maior lista.
+# Para utilizar esse zip e preciso importar 
 # e no caso de uma lista menor será adicionado 'None' nos espaços em falta
 # podendo ser substituido o 'None' por qualquer coisa com a função: 'fillvalue'= "palavra escolhida" 
 
