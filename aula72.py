25a06fd8e3549b2ef0e2b32e9c191983417b9944
joselisboaa <joselisboaa05@gmail.com>
2022-09-20 13:03:42 -0300

    comportamento de geradores e iteradores

---
+++ aula72.py
@@ -0,0 +1,35 @@
+"""
+Comportamento de geradores e iteradores
+"""
+
+nome = 'miguel santos'
+iterador = iter(nome)
+gerador = (letra for letra in nome)
+
+print(next(gerador))
+print(next(gerador))
+print(next(gerador))
+print(next(gerador))
+print(next(gerador))
+print(next(gerador))
+print(next(gerador))
+print('for agora')
+for letra in gerador:
+    print(letra)
+print(next(gerador)) #da mesma forma dos iteradores o gerador também irá
+# dar erro quando o valor for consumido 
+
+# print(next(iterador))
+# print(next(iterador))
+# print(next(iterador))
+# print(next(iterador))
+# print(next(iterador))
+# print(next(iterador))
+# print(next(iterador))
+# print(next(iterador))
+# print(next(iterador))
+# print(next(iterador))
+# print(next(iterador))
+# print(next(iterador))
+# print(next(iterador)) # aqui foi consumido por completo o iterador  
+# print(next(iterador)) # aqui irá da erro por não ter mais o que consumir 
