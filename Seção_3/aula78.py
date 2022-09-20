027e14e6d729d5078ca73946d2b82634c853269c
joselisboaa <miguel.santoss085@gmail.com>
2022-09-20 15:36:00 -0300

    Count - contadores infinitos

---
+++ Seção_3/aula78.py
@@ -0,0 +1,15 @@
+"""
+Count - contadores infinitos
+"""
+
+from itertools import count
+# preciso importar para usar o count
+
+contador = count(start = 0, step = 1)
+# start = de onde quer começar
+# step = de quanto em quanto eu quero que pule
+
+for teste in contador:
+    print(teste)
+    if teste == 10:
+        break
