7a855859fb6e4641e1101dd9765482f435d141b5
Miguel Santos <miguel.santoss085@gmail.com>
2022-09-22 13:17:28 -0300

    Map

--- Seção_3/aula81.py
+++ Seção_3/aula81.py
@@ -2,30 +2,21 @@
 Map - Dados
 """
 
-produto = [
-    {'nome': 'p1', 'preco': 56},
-    {'nome': 'p2', 'preco': 48},
-    {'nome': 'p3', 'preco': 54},
-    {'nome': 'p4', 'preco': 21},
-    {'nome': 'p5', 'preco': 34},
-    {'nome': 'p6', 'preco': 57},
-    {'nome': 'p7', 'preco': 98},
-    {'nome': 'p8', 'preco': 11},
-    {'nome': 'p9', 'preco': 13},
-    {'nome': 'p10', 'preco': 97},
-]
+from aula81dados import produto, pessoas, lista 
 
-pessoas = [
-    {'nome': 'Marcos', 'idade': 26},
-    {'nome': 'Paulo', 'idade': 30},
-    {'nome': 'João', 'idade': 18},
-    {'nome': 'Eduarda', 'idade': 25},
-    {'nome': 'Marcela', 'idade': 37},
-    {'nome': 'Jonas', 'idade': 40},
-    {'nome': 'Maria', 'idade': 16},
-    {'nome': 'Pamela', 'idade': 53},
-    {'nome': 'Roberta', 'idade': 47},
-    {'nome': 'Michael', 'idade': 35},
-]
+# print(lista)
+# print(pessoas)
+# print(produto)
+# for teste in pessoas:
+#     print(teste)
 
-lista = [1,2,3,4,5,6,7]
+# nova_lista = [x  for x in lista]
+# print(nova_lista)
+
+
+# forma de pegar algo especifico de um dicionario usando o map eo lambda
+
+nomes = map(lambda p: p['nome'], pessoas)
+
+for nomes in nomes:
+    print(nomes)
---
+++ Seção_3/aula81dados.py
@@ -0,0 +1,31 @@
+"""
+Map - Dados
+"""
+# Dados para serem importados
+produto = [
+    {'nome': 'p1', 'preco': 56},
+    {'nome': 'p2', 'preco': 48},
+    {'nome': 'p3', 'preco': 54},
+    {'nome': 'p4', 'preco': 21},
+    {'nome': 'p5', 'preco': 34},
+    {'nome': 'p6', 'preco': 57},
+    {'nome': 'p7', 'preco': 98},
+    {'nome': 'p8', 'preco': 11},
+    {'nome': 'p9', 'preco': 13},
+    {'nome': 'p10', 'preco': 97},
+]
+
+pessoas = [
+    {'nome': 'Marcos', 'idade': 26},
+    {'nome': 'Paulo', 'idade': 30},
+    {'nome': 'João', 'idade': 18},
+    {'nome': 'Eduarda', 'idade': 25},
+    {'nome': 'Marcela', 'idade': 37},
+    {'nome': 'Jonas', 'idade': 40},
+    {'nome': 'Maria', 'idade': 16},
+    {'nome': 'Pamela', 'idade': 53},
+    {'nome': 'Roberta', 'idade': 47},
+    {'nome': 'Michael', 'idade': 35},
+]
+
+lista = [1,2,3,4,5,6,7]
