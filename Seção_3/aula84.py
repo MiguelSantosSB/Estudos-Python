299fc04788d6a38c25d969b908c8f35df72d428c
Miguel Santos <miguel.santoss085@gmail.com>
2022-09-22 15:06:03 -0300

    Try e Except

---
+++ Seção_3/aula84.py
@@ -0,0 +1,22 @@
+"""
+Try e Except
+Tratando Exceções em Python
+"""
+
+# Formato mais simples trantando uma exceção
+# try:
+#     print(a)
+# except:
+#     print('Erro')
+# normalmente isso não iria rodar pois o 'a' não foi declarado
+# porém, por não ter 'rodado', ele pula para o except e roda oque 
+# está no except.
+
+try:
+    print(a)
+except NameError as erro:
+    print('erro',erro)
+except IndexError as erro:
+    print('erro',erro)
+except SyntaxError as erro:
+    print('erro',erro)
