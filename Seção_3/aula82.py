d70a814844fb826c40b113adbf80bd896fcfb8e4
Miguel Santos <miguel.santoss085@gmail.com>
2022-09-22 13:43:20 -0300

    Filter

--- Seção_3/aula82.py
+++ Seção_3/aula82.py
@@ -8,11 +8,12 @@ from aula81dados import produto, pessoas, lista
 # nova_lista = filter(lambda x: x > 5, lista)
 # print(list(nova_lista))
 
+# função que irá pegar os números da lista maiores que 5
 def filtra(dado):
     if dado > 5:
         dado = True
         return True
-
+# função que irá pegar os preços maiores que 50
 def filtra2(dado):
     if dado['preco'] > 50:
         dado['certo'] = True
