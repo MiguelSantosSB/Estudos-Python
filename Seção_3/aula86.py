81b056798fee96bdf08c758b89f61fcf5dccf902
Miguel Santos <miguel.santoss085@gmail.com>
2022-09-22 15:40:36 -0300

    Uso de Try e Except como Condicional

---
+++ Seção_3/aula86.py
@@ -0,0 +1,21 @@
+"""
+Uso de Try e Except como Condicional
+"""
+
+def converte_numero(valor):
+    try:
+        valor = int(valor)
+        return valor
+    except ValueError:
+        try:
+            valor = float(valor)
+            return valor
+        except ValueError:
+            pass
+while True:
+
+    numero = input('Digite um número: ')
+    if numero is None:
+        print('Error: isso não e um numero.')     
+    else:
+        print(numero * 2)
