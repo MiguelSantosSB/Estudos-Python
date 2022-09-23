0139ce44b52cbf27c85020fba913190c541c6458
Miguel Santos <miguel.santoss085@gmail.com>
2022-09-23 20:27:07 -0300

    Funções Decoradoras e Decoradores

--- Seção_3/aula92.py
+++ Seção_3/aula92.py
@@ -1,13 +1,37 @@
 """
 Funções Decoradoras e Decoradores
 """
+#  funções decoradoras são funções que involvem outras funções, ou seja, uma função dentro de uma função
 
+# def fala_oi():
+#     print('oi')
 
+# variavel = fala_oi
+# variavel()
 
+# def master():
+#     def slave():
+#         print('oi')
+#     return slave()
+# uma função que tem uma função escrava(função que precisa ser chamada para poder realizar o que for pedido)
+# variavel = master()
+# variavel()
+# print(type(variavel))
 
+# FUNÇÃO DECORADORA
+def master(funcao):
+    def slave():
+        print('decorando...')
+        funcao()
+    return slave
+# uma função que irá recebe outra função para realiza-la 
 
+@master
+# coloca a função dentro da função decoradora
+def fala_oi():
+    print('oi')
 
-
-
-
-
+variavel = master(fala_oi)
+# eh como se tivesse executando essa função diretamente
+# variavel()
+fala_oi()
