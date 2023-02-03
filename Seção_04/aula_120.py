16b8ac0e1bb12f3d0a8462ed8487ac2b37b6641c
Miguel Santos <111910150+MiguelSantosSB@users.noreply.github.com>
2023-02-03 15:08:50 -0300

    Métodos úteis dos dicionários em Python

---
+++ Seção_04/aula_120.py
@@ -0,0 +1,25 @@
+'''
+Métodos úteis dos dicionários em Pytyhon
+
+    len - quantas chaves
+    kays - iterável com as chaves 
+    values  - iterável com os valores
+    items - iterável com chaves e valores
+    setdefault - adiciona valor se a chave não existe 
+    copy - retorna uma cópia rasa(shallow copy)
+    get - obtém uma chave
+    pop - apaga um item com a chave especificada(del)
+
+
+
+
+
+
+
+
+
+
+'''
+
+
+
