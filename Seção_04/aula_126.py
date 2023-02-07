1f8c217cf9e8bdb0b448c5b9181f73276e017a8a
Miguel Santos <miguel.santoss085@gmail.com>
2023-02-07 00:37:04 -0300

    Tipo set em Python

---
+++ Seção_04/aula_126.py
@@ -0,0 +1,11 @@
+'''
+Sets em Python são mutáveis, porém aceitam apenas tipos 
+imutáveis como: listas, dicionários e etc.
+
+Representados graficamente pelo diagrama de Venn
+'''
+
+# Criando um Set
+# set(iterável) ou {1,2,3}
+s1 = set()
+print(s1, type(s1))
